52da145b5096fb94827d94cadab9d73fdbafb8a5
João Koritar <jakoritarleite@gmail.com>
2023-03-31 12:08:22 -0300

    Implement Entity trait for Player

--- src/player.rs
+++ src/player.rs
@@ -1,9 +1,14 @@
+use ggez::glam::Vec2;
+use ggez::graphics::Canvas;
 use ggez::graphics::Color;
 use ggez::graphics::DrawMode;
 use ggez::graphics::Mesh;
 use ggez::graphics::Rect;
+use ggez::input::keyboard::KeyCode;
 use ggez::Context;
 use ggez::GameError;
+
+use crate::Entity;
 
 pub struct Player {
     pub pos_x: f32,
@@ -20,5 +25,25 @@ impl Player {
             pos_x: (ctx.gfx.size().0 / 2.0) - (rect.w / 2.0),
             shape,
         })
+    }
+}
+
+impl Entity for Player {
+    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        if ctx.keyboard.is_key_pressed(KeyCode::A) {
+            self.pos_x -= 1.0;
+        }
+
+        if ctx.keyboard.is_key_pressed(KeyCode::D) {
+            self.pos_x += 1.0;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&self, ctx: &Context, canvas: &mut Canvas) -> Result<(), GameError> {
+        canvas.draw(&self.shape, Vec2::new(self.pos_x, ctx.gfx.size().1 - 150.0));
+
+        Ok(())
     }
 }
