d733172b9b984915554d9e30ae653d09ee1150f2
João Koritar <jakoritarleite@gmail.com>
2023-03-31 12:08:15 -0300

    Create trait that implements methods to update and draw entities

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+use ggez::{graphics::Canvas, Context, GameError};
+
 pub mod player;
 pub mod state;
+
+trait Entity {
+    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError>;
+
+    fn draw(&self, ctx: &Context, canvas: &mut Canvas) -> Result<(), GameError>;
+}
