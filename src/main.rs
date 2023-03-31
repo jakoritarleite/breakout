5908b3f77fbfa7f50eef2d0d4820ef3a084efe84
João Koritar <jakoritarleite@gmail.com>
2023-03-31 09:55:13 -0300

    Create basic game event_loop and context

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use ggez::{
+    event::{self, EventHandler},
+    graphics::{self, Color},
+    Context, ContextBuilder, GameError,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let (mut ctx, event_loop) = ContextBuilder::new("breakout", "Joao Koritar")
+        .build()
+        .expect("Could not create game context");
+
+    let breakout = Breakout::new(&mut ctx);
+
+    event::run(ctx, event_loop, breakout);
+}
+
+struct Breakout;
+
+impl Breakout {
+    pub fn new(ctx: &mut Context) -> Self {
+        Self
+    }
+}
+
+impl EventHandler for Breakout {
+    fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+
+        canvas.finish(ctx)
+    }
 }
