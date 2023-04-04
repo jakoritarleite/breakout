8f963e2287f7048801061e0d42828c4eae85e389
João Koritar <jakoritarleite@gmail.com>
2023-04-04 11:41:33 -0300

    Implement function to clamp at X or Y coordinates

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use ggez::{graphics::Canvas, Context, GameError};
 
+pub mod balls;
+pub mod blocks;
 pub mod player;
 pub mod state;
 
@@ -7,4 +9,12 @@ trait Entity {
     fn update(&mut self, ctx: &mut Context) -> Result<(), GameError>;
 
     fn draw(&self, ctx: &Context, canvas: &mut Canvas) -> Result<(), GameError>;
+
+    fn clamp(coord: &mut f32, low: f32, high: f32) {
+        if *coord < low {
+            *coord = low;
+        } else if *coord > high {
+            *coord = high;
+        }
+    }
 }
