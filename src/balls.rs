4d08e42b7056b8f38402c4ed6a0d0568603d04ad
João Koritar <jakoritarleite@gmail.com>
2023-04-04 11:41:11 -0300

    Create ball entity

---
+++ src/balls.rs
@@ -0,0 +1,84 @@
+use ggez::glam::vec2;
+use ggez::glam::Vec2;
+use ggez::graphics::Color;
+use ggez::graphics::DrawMode;
+use ggez::graphics::Mesh;
+use ggez::Context;
+use ggez::GameError;
+use rand::Rng;
+
+use crate::Entity;
+
+const BALL_SPEED: f32 = 5.0;
+const BALL_RADIUS: f32 = 10.0;
+
+pub struct Ball {
+    pub vel: Vec2,
+    pub pos_x: f32,
+    pub pos_y: f32,
+    pub shape: Mesh,
+}
+
+impl Ball {
+    pub fn new(ctx: &mut Context) -> Result<Ball, GameError> {
+        let shape = Mesh::new_circle(
+            ctx,
+            DrawMode::fill(),
+            vec2(-BALL_RADIUS, -BALL_RADIUS),
+            BALL_RADIUS,
+            1.0,
+            Color::WHITE,
+        )?;
+
+        let origin = (
+            ctx.gfx.size().0 / 2.0 - BALL_RADIUS,
+            ctx.gfx.size().1 - 150.0,
+        );
+
+        Ok(Self {
+            vel: vec2(
+                rand::thread_rng().gen_range(-1.0..1.0),
+                rand::thread_rng().gen_range(-1.0..1.0),
+            )
+            .normalize(),
+            pos_x: origin.0,
+            pos_y: origin.1,
+            shape,
+        })
+    }
+}
+
+impl Entity for Ball {
+    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        self.pos_x += self.vel.x * BALL_SPEED;
+        self.pos_y += self.vel.y * BALL_SPEED;
+
+        if self.pos_x < BALL_RADIUS {
+            self.vel.x = 1.0;
+        }
+
+        if self.pos_x > ctx.gfx.size().0 - BALL_RADIUS {
+            self.vel.x = -1.0;
+        }
+
+        if self.pos_y < BALL_RADIUS {
+            self.vel.y = 1.0;
+        }
+
+        if self.pos_y > ctx.gfx.size().1 - BALL_RADIUS {
+            self.vel.y = -1.0;
+        }
+
+        Ok(())
+    }
+
+    fn draw(
+        &self,
+        _ctx: &ggez::Context,
+        canvas: &mut ggez::graphics::Canvas,
+    ) -> Result<(), GameError> {
+        canvas.draw(&self.shape, Vec2::new(self.pos_x, self.pos_y));
+
+        Ok(())
+    }
+}
