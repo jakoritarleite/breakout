a577e1923432283cc7241a65eb7760300507518e
João Koritar <jakoritarleite@gmail.com>
2023-04-04 11:42:33 -0300

    Update player to have its pivot point in the middle of the rect, set it a speed and clamp it in the X coordinates

--- src/player.rs
+++ src/player.rs
@@ -1,3 +1,6 @@
+use crate::balls::Ball;
+use crate::Entity;
+use ggez::glam::vec2;
 use ggez::glam::Vec2;
 use ggez::graphics::Canvas;
 use ggez::graphics::Color;
@@ -8,34 +11,63 @@ use ggez::input::keyboard::KeyCode;
 use ggez::Context;
 use ggez::GameError;
 
-use crate::Entity;
+const PLAYER_WIDTH: f32 = 170f32;
+const PLAYER_HEIGHT: f32 = 30f32;
 
 pub struct Player {
+    speed: Vec2,
     pub pos_x: f32,
     pub shape: Mesh,
+    pub shots: Vec<Ball>,
 }
 
 impl Player {
     pub fn new(ctx: &mut Context) -> Result<Player, GameError> {
-        let rect = Rect::new(0.0, 0.0, 170.0, 30.0);
+        let rect = Rect::new(
+            -PLAYER_WIDTH * 0.5,
+            -PLAYER_HEIGHT * 0.5,
+            PLAYER_WIDTH,
+            PLAYER_HEIGHT,
+        );
         let shape = Mesh::new_rectangle(ctx, DrawMode::fill(), rect, Color::WHITE)?;
 
         Ok(Self {
+            speed: vec2(300.0, 0.0),
             // Align it in the middle of the screen
-            pos_x: (ctx.gfx.size().0 / 2.0) - (rect.w / 2.0),
+            pos_x: (ctx.gfx.size().0 * 0.5),
             shape,
+            shots: vec![],
         })
     }
 }
 
 impl Entity for Player {
     fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        let dt = ctx.time.delta().as_secs_f32();
+
         if ctx.keyboard.is_key_pressed(KeyCode::A) {
-            self.pos_x -= 1.0;
+            self.pos_x -= self.speed.x * dt;
         }
 
         if ctx.keyboard.is_key_pressed(KeyCode::D) {
-            self.pos_x += 1.0;
+            self.pos_x += self.speed.x * dt;
+        }
+
+        if ctx.keyboard.is_key_pressed(KeyCode::Space) {
+            self.shots.push(Ball::new(ctx)?);
+        }
+
+        let player_width_half = PLAYER_WIDTH * 0.5;
+
+        Self::clamp(
+            &mut self.pos_x,
+            player_width_half,
+            ctx.gfx.size().0 - player_width_half,
+        );
+
+        // Loop over all balls
+        for shot in self.shots.iter_mut() {
+            shot.update(ctx)?;
         }
 
         Ok(())
@@ -43,6 +75,11 @@ impl Entity for Player {
 
     fn draw(&self, ctx: &Context, canvas: &mut Canvas) -> Result<(), GameError> {
         canvas.draw(&self.shape, Vec2::new(self.pos_x, ctx.gfx.size().1 - 150.0));
+
+        // Loop over all balls
+        for shot in self.shots.iter() {
+            shot.draw(ctx, canvas)?;
+        }
 
         Ok(())
     }
