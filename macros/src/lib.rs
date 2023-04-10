fff716515de129cea3302cdbea5cc720004fa8d8
João Koritar <jakoritarleite@gmail.com>
2023-04-10 16:17:14 -0300

    Create derive proc macros to Component

---
+++ macros/src/lib.rs
@@ -0,0 +1,36 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+use syn_path::path;
+
+#[proc_macro_derive(Component)]
+pub fn derive_component(input: TokenStream) -> TokenStream {
+    //let ast = parse_macro_input!(input as DeriveInput);
+    let ast: syn::DeriveInput = syn::parse(input).unwrap();
+
+    let struct_name = &ast.ident;
+    // let (impl_generics, type_generics, where_clause) = &ast.generics.split_for_impl();
+
+    let breakout_path = path!(crate::ecs);
+
+    TokenStream::from(quote! {
+        impl #breakout_path::component::Component for #struct_name {}
+
+        impl #breakout_path::component::Bundle for #struct_name {
+            fn components_ids(
+                &self,
+                entity: #breakout_path::entity::Entity,
+                components: &mut #breakout_path::component::Components,
+                storages: &mut #breakout_path::world::Storages,
+                ids: &mut impl FnMut(#breakout_path::component::ComponentId)
+            ) {
+                ids(components.init_component::<#struct_name>(storages));
+                storages.push_component(
+                    entity.clone(),
+                    std::any::TypeId::of::<#struct_name>(),
+                    self.clone()
+                );
+            }
+        }
+    })
+}
