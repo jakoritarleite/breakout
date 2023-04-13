f1fd008b4a12fc1e335de7a8852bc6b649c66104
João Koritar <jakoritarleite@gmail.com>
2023-04-13 15:12:11 -0300

    Remove old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,3 @@
-use ggez::glam::Vec2;
-use ggez::graphics::Canvas;
-use ggez::Context;
-use ggez::GameError;
-
-pub mod ball;
-pub mod block;
 pub mod ecs;
 pub mod geometry;
 pub mod physics;
-pub mod player;
-pub mod state;
-
-pub trait Entity {
-    fn velocity(&self) -> Vec2;
-
-    fn position(&self) -> Vec2;
-
-    fn dimensions(&self) -> (f32, f32);
-
-    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError>;
-
-    fn draw(&self, ctx: &Context, canvas: &mut Canvas) -> Result<(), GameError>;
-
-    fn clamp(coord: &mut f32, low: f32, high: f32) {
-        if *coord < low {
-            *coord = low;
-        } else if *coord > high {
-            *coord = high;
-        }
-    }
-}
