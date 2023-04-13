c4770d1067fe09ff49a4602f8c608892f94abcc1
João Koritar <jakoritarleite@gmail.com>
2023-04-13 15:10:02 -0300

    Fix intersection algorithm

--- src/geometry.rs
+++ src/geometry.rs
@@ -9,11 +9,11 @@ pub fn intersection(
     dim_b_w: f32,
     dim_b_h: f32,
 ) -> Option<(f32, f32, f32, f32)> {
-    let left = (pos_a.x - dim_a_w).max(pos_b.x - dim_b_w);
-    let top = (pos_a.y - dim_a_h).max(pos_b.y - dim_b_h);
+    let left = pos_a.x.max(pos_b.x);
+    let top = pos_a.y.max(pos_b.y);
 
     let right = (pos_a.x + dim_a_w).min(pos_b.x + dim_b_w);
-    let bottom = (pos_a.y + dim_a_w).min(pos_b.y + dim_b_h);
+    let bottom = (pos_a.y + dim_a_h).min(pos_b.y + dim_b_h);
 
     // (x, y, w, h)
     let intersection = (left, top, right - left, bottom - top);
